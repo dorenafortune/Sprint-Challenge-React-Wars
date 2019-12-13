2b88b2dd9190faf2a8158fd2525e751e43412e4a
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-13 10:20:48 -0800

    finished readme questions

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,6 +1,15 @@
 import React from "react";
 import styled from "styled-components"
 
+const Card = styled.div`
+
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+
+`
+
 const ColorH2 = styled.h2`
 color: #198CFF;
 font-family:'Abel', sans-serif;
@@ -9,18 +18,24 @@ text-shadow: 3px 2px red;
 
 `;
 
+const ColorH3 = styled.h3`
+color: #FEFEBE;
+text-shadow: 3px 2px black;
+
+`
+
 
 
 const PeopleCard = props => {
     console.log(props)
     return (
-        <div className="person-data" key={props.peoples.name}>
+        <Card key={props.peoples.name}>
             <ColorH2>{props.name}</ColorH2>
-            <h3>Birth Year: {props.birthyear}</h3>
-            <h3>Height:{props.height}</h3>
-            <h3>Gender:{props.gender}</h3>
+            <ColorH3>Birth Year: {props.birthyear}</ColorH3>
+            <ColorH3>Height:{props.height}</ColorH3>
+            <ColorH3>Gender:{props.gender}</ColorH3>
             
-        </div>
+        </Card>
 
     )
 }
